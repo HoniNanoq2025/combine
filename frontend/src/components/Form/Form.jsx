ef0f91cdbacb1ef9e78042a811966b88b5e42cd1
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-09-29 14:44:42 +0200

    update

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,26 +1,59 @@
-import React from "react";
-import styles from './Form.module.css'
+import React, { useState } from "react";
+import styles from "./Form.module.css";
 
-export default function Form(){
-    return (
-      <div className={styles.container}>
-        <form className={styles.form}>
-          <label htmlFor="name">name</label>
-          <input type="text" name="name" id="name" className={styles.input} />
+export default function Form() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
 
-          <label htmlFor="description">description</label>
-          <input type="text" name="description" id="description" className={styles.input} />
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-          <button type="submit" className={styles.btn}>
-            send data
-          </button>
+    try {
+      const response = await fetch("http://localhost:3000/items", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, description }),
+      });
 
-        </form>
+      const data = await response.json();
+      console.log("Item oprettet", data);
 
-        
+      //Nulstil form
+      setName("");
+      setDescription("");
+    } catch (error) {
+      console.error("Fejl", error);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label htmlFor="name">name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className={styles.input}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
+        <label htmlFor="description">description</label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          className={styles.input}
+          value={name}
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
-      </div>
-    );
-
+        <button type="submit" className={styles.btn}>
+          send data
+        </button>
+      </form>
+    </div>
+  );
 }
